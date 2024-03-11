6192988afb3779fed27fa1656c0ad47200475f5a
Davide Orlandi <davide05orlandi@gmail.com>
2024-03-11 19:12:52 +0100

    impostato il valore di default per poter modificare il nome

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,7 +9,7 @@ const Player = ({name, symbol}) => {
 
   let playerName = <span className="player-name">{name}</span>;
   if(isEditing) {
-    playerName =  <input type='text' required value={name} /> ;
+    playerName =  <input type='text' required defaultValue={name} /> ;
   }
 
   return (
